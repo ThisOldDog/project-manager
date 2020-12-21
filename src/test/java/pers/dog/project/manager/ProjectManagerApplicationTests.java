34c04ca2564140e5eec5fece4cff5fe8259ac93f
废柴 <ThisOldDog@163.com>
2020-12-21 20:58:18 +0800

    [init]

---
+++ src/test/java/pers/dog/project/manager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.dog.project.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
