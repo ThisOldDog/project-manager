a366a8e217b5e9f2269958d0a32d03af6dd4a010
废柴 <ThisOldDog@163.com>
2021-02-01 09:05:43 +0800

    [FIX] MyBatis Plus 分页

--- src/main/java/pers/dog/project/manager/ProjectManagerApplication.java
+++ src/main/java/pers/dog/project/manager/ProjectManagerApplication.java
@@ -1,9 +1,12 @@
 package pers.dog.project.manager;
 
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import pers.dog.project.manager.configuration.GitlabProperties;
 import pers.dog.project.manager.configuration.IamProperties;
 
@@ -14,6 +17,13 @@ import pers.dog.project.manager.configuration.IamProperties;
 })
 @MapperScan("pers.dog.project.manager.mapper")
 public class ProjectManagerApplication {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagerApplication.class, args);
